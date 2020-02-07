2f5672b3d25750fa5ab3c0e94a8d0314f9052cbb
Khurram Shehzad <khurram.pak522@gmail.com>
2020-02-07 10:27:39 +0500

    Show clap button along with a circular progress bar

--- app/src/main/java/com/example/clapy/MainActivity.kt
+++ app/src/main/java/com/example/clapy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.clapy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// Clap image taken from Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
